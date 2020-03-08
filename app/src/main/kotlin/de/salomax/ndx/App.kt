5dca9c02070e503ec1aae59b5f2a246962c94f3d
Max <msal.coding@gmail.com>
2020-03-08 02:22:55 +0100

    Migrated to LeakCanary 2

--- app/src/main/kotlin/de/salomax/ndx/App.kt
+++ app/src/main/kotlin/de/salomax/ndx/App.kt
@@ -1,21 +1,12 @@
 package de.salomax.ndx
 
 import android.app.Application
-import com.squareup.leakcanary.LeakCanary
 
+@Suppress("unused")
 class App : Application() {
 
+    @Suppress("RedundantOverride")
     override fun onCreate() {
         super.onCreate()
-
-        // init leakCanary
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        if (BuildConfig.DEBUG) {
-            LeakCanary.install(this)
-        }
     }
 }
