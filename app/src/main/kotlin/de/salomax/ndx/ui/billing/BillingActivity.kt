1befb27a5adc418eb9260e65cb5ad26487ab74e7
Max <msal.coding@gmail.com>
2020-11-26 03:10:32 +0100

    another one…

--- app/src/main/kotlin/de/salomax/ndx/ui/billing/BillingActivity.kt
+++ app/src/main/kotlin/de/salomax/ndx/ui/billing/BillingActivity.kt
@@ -6,11 +6,8 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.android.billingclient.api.*
 import com.android.billingclient.api.BillingClient.BillingResponseCode
-import com.google.android.material.snackbar.Snackbar
 import de.salomax.ndx.R
 import de.salomax.ndx.ui.BaseActivity
-import de.salomax.ndx.util.Logger
-import kotlinx.android.synthetic.main.activity_billing.*
 
 /**
 Billing workflow:
@@ -20,7 +17,8 @@ Billing workflow:
 4. evaluate result of the buy: #onPurchasesUpdated()
   a) accept buy:               #acknowledgePurchase() -> for non-consumable
  */
-class BillingActivity : BaseActivity() {
+class BillingActivity : BaseActivity(), PurchasesUpdatedListener, BillingClientStateListener,
+      SkuDetailsResponseListener, AcknowledgePurchaseResponseListener {
 
    private var billingClient: BillingClient? = null
 
@@ -47,7 +45,7 @@ class BillingActivity : BaseActivity() {
 
       // button
       findViewById<Button>(R.id.btn_billing_buy).setOnClickListener {
-         buy()
+         startPaymentFlow()
       }
    }
 
@@ -62,125 +60,94 @@ class BillingActivity : BaseActivity() {
       billingClient = null
    }
 
-   // 1. connect client
-   private fun buy() {
-      // connect
-      billingClient?.startConnection(object : BillingClientStateListener {
-         override fun onBillingSetupFinished(billingResponse: BillingResult) {
-            when (billingResponse.responseCode) {
-               BillingResponseCode.OK -> {
-                  // The BillingClient is ready. You can query purchases here.
-                  Logger.log("startConnection        | The BillingClient is ready. You can query purchases here.")
-                  querySkuDetails()
-               }
-               BillingResponseCode.ITEM_ALREADY_OWNED -> {
-                  Logger.log("querySkuDetailsAsync   | item already owned: premium granted")
-                  ViewModelProvider(this@BillingActivity).get(BillingViewModel::class.java).enablePremium()
-                  thanksAndFinish()
-               }
-               else -> {
-                  Snackbar.make(btn_billing_buy, billingResponse.debugMessage, Snackbar.LENGTH_LONG).setBackgroundTint(getColor(android.R.color.holo_red_light)).show()
-                  Logger.log("startConnection        | Error: ${billingResponse.debugMessage} (${billingResponse.responseCode})")
-               }
-            }
-         }
-
-         override fun onBillingServiceDisconnected() {
-            // try to restart the connection on the next request to Google Play by calling the startConnection() method.
-            billingClient?.startConnection(this)
-            Logger.log("onBillingServiceDisconnected   | Disconnected")
-         }
-      })
-   }
-
-   // 2. get details of all SKUs
-   private fun querySkuDetails() {
-      if (billingClient?.isReady == true) {
-         val params = SkuDetailsParams
-               .newBuilder()
-               .setSkusList(listOf("premium")) // non-consumable
-               .setType(BillingClient.SkuType.INAPP)
-               .build()
-         billingClient?.querySkuDetailsAsync(params) { billingResult, skuDetails ->
-            when (billingResult.responseCode) {
-               BillingResponseCode.OK -> {
-                  Logger.log("querySkuDetailsAsync   | response: ${billingResult.responseCode} -> launchBillingFLow")
-                  launchBillingFlow(skuDetails as MutableList<SkuDetails>)
-               }
-               BillingResponseCode.ITEM_ALREADY_OWNED -> {
-                  Logger.log("querySkuDetailsAsync   | item already owned: premium granted")
-                  ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
-                  thanksAndFinish()
-               }
-               else -> {
-                  Logger.log("querySkuDetailsAsync   | Can't do: ${billingResult.debugMessage}")
-               }
-            }
-         }
-      } else {
-         Logger.log("BillingClient not ready")
-      }
-   }
-
-   // 3. buy something
-   private fun launchBillingFlow(skuDetails: List<SkuDetails>) {
-      for (skuDetail in skuDetails) {
-         Logger.log("launchBillingFlow      | ${skuDetail.originalJson}")
-         billingClient?.launchBillingFlow(this, BillingFlowParams.newBuilder().setSkuDetails(skuDetail).build())
-      }
+   private fun startPaymentFlow() {
+      billingClient?.startConnection(this)
    }
 
    // 4. evaluate result of the buy...
-   private fun onPurchasesUpdated(billingResult: BillingResult?, purchases: MutableList<Purchase>?) {
+   override fun onPurchasesUpdated(billingResult: BillingResult, purchases: MutableList<Purchase>?) {
       when {
-         billingResult?.responseCode == BillingResponseCode.OK && purchases != null -> {
-            for (purchase in purchases)
+         billingResult.responseCode == BillingResponseCode.OK && purchases != null -> {
+            for (purchase in purchases) {
+               // This call only returns unacknowledged purchases
                if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
-                  Logger.log("onPurchasesUpdated     | premium granted")
-                  ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
-                  // ...and acknowledge the purchase if it hasn't already been acknowledged.
-                  if (!purchase.isAcknowledged) {
-                     acknowledgePurchase(purchase.purchaseToken)
-                  }
+                  val params = AcknowledgePurchaseParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+                        .build()
+                  billingClient?.acknowledgePurchase(params, this)
                }
-         }
-         billingResult?.responseCode == BillingResponseCode.ITEM_ALREADY_OWNED && purchases != null -> {
-            Logger.log("querySkuDetailsAsync   | item already owned: premium granted")
+            }
             ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
             thanksAndFinish()
          }
-         else -> {
-            // handle any error code (e. g. by a user cancelling the purchase flow)
-            Logger.log("onPurchasesUpdated     | ${billingResult?.responseCode}")
+         billingResult.responseCode == BillingResponseCode.ITEM_ALREADY_OWNED -> {
+            ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
+            thanksAndFinish()
+         }
+         billingResult.responseCode == BillingResponseCode.USER_CANCELED -> {
+            // TODO ?
          }
       }
    }
 
-   // 4. a) accept buy
-   private fun acknowledgePurchase(purchaseToken: String) {
-      val params = AcknowledgePurchaseParams.newBuilder()
-            .setPurchaseToken(purchaseToken)
-            .build()
-      billingClient?.acknowledgePurchase(params) { billingResult ->
-         when (billingResult.responseCode) {
-            BillingResponseCode.OK -> {
-               thanksAndFinish()
-            }
-            BillingResponseCode.ITEM_ALREADY_OWNED -> {
-               Logger.log("querySkuDetailsAsync   | item already owned: premium granted")
-               ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
-               thanksAndFinish()
-            }
-            else -> {
-               Logger.log("acknowledgePurchase    | failure; billingResult: ${billingResult.responseCode}")
-            }
-         }
+   override fun onAcknowledgePurchaseResponse(billingResult: BillingResult) {
+      if (billingResult.responseCode == BillingResponseCode.ITEM_ALREADY_OWNED) {
+         ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
+         thanksAndFinish()
       }
    }
 
    private fun thanksAndFinish() {
       Toast.makeText(applicationContext, getString(R.string.billing_thanks_for_buying), Toast.LENGTH_LONG).show()
       onSupportNavigateUp()
+   }
+
+   override fun onBillingSetupFinished(billingResult: BillingResult) {
+      if (billingResult.responseCode == BillingResponseCode.ITEM_ALREADY_OWNED) {
+         ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
+         thanksAndFinish()
+         return
+      }
+
+      // Acknowledge any purchases that haven't been acknowledged already
+      val purchases = billingClient?.queryPurchases(BillingClient.SkuType.INAPP)
+      purchases?.purchasesList?.forEach { purchase ->
+         if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
+            if (purchase.isAcknowledged) {
+               val params = AcknowledgePurchaseParams.newBuilder()
+                     .setPurchaseToken(purchase.purchaseToken)
+                     .build()
+               billingClient?.acknowledgePurchase(params, this)
+            }
+            ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
+            thanksAndFinish()
+         }
+      }
+
+      val params = SkuDetailsParams
+            .newBuilder()
+            .setSkusList(listOf("premium")) // non-consumable
+            .setType(BillingClient.SkuType.INAPP)
+            .build()
+      billingClient?.querySkuDetailsAsync(params, this)
+   }
+
+   override fun onBillingServiceDisconnected() {
+      // TODO failed
+   }
+
+   override fun onSkuDetailsResponse(billingResult: BillingResult, skuDetailsList: MutableList<SkuDetails>?) {
+      if (billingResult.responseCode == BillingResponseCode.ITEM_ALREADY_OWNED) {
+         ViewModelProvider(this).get(BillingViewModel::class.java).enablePremium()
+         thanksAndFinish()
+      } else if (billingResult.responseCode != BillingResponseCode.OK) {
+         // TODO failed
+         return
+      }
+
+      skuDetailsList?.forEach { skuDetails ->
+         billingClient?.launchBillingFlow(this, BillingFlowParams.newBuilder().setSkuDetails(skuDetails).build())
+      }
    }
 
 }
