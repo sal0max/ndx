a15d3374d10368f5004733671b30ddd679f87216
Max <msal.coding@gmail.com>
2020-11-27 22:22:06 +0100

    some more refactoring

--- app/src/main/kotlin/de/salomax/ndx/data/model/IsoSteps.kt
+++ app/src/main/kotlin/de/salomax/ndx/data/model/IsoSteps.kt
@@ -2,41 +2,40 @@ package de.salomax.ndx.data.model
 
 enum class IsoSteps(val values: IntArray) {
 
-    FULL(
-            intArrayOf(
-                    100,
-                    200,
-                    400,
-                    800,
-                    1600,
-                    3200,
-                    6400)
-    ),
+   FULL(
+         intArrayOf(
+                100,
+                200,
+                400,
+                800,
+               1600,
+               3200,
+               6400)
+   ),
 
-    HALF(
-            intArrayOf(
-                    100, 140,
-                    200, 280,
-                    400, 560,
-                    800, 1100,
-                    1600, 2200,
-                    3200, 4500,
-                    6400, 9000
-            )
-    ),
+   HALF(
+         intArrayOf(
+                100,  140,
+                200,  280,
+                400,  560,
+                800, 1100,
+               1600, 2200,
+               3200, 4500,
+               6400, 9000)
+   ),
 
-    THIRD(
-            intArrayOf(
-                    100, 125, 160,
-                    200, 250, 320,
-                    400, 500, 640,
-                    800, 1000, 1250,
-                    1600, 2000, 2500,
-                    3200, 4000, 5000,
-                    6400, 8000, 10000)
-    );
+   THIRD(
+         intArrayOf(
+                100,  125,   160,
+                200,  250,   320,
+                400,  500,   640,
+                800, 1000,  1250,
+               1600, 2000,  2500,
+               3200, 4000,  5000,
+               6400, 8000, 10000)
+   );
 
-    operator fun get(position: Int): Int {
-        return values[position]
-    }
+   operator fun get(position: Int): Int {
+      return values[position]
+   }
 }
