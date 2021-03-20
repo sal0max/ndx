25e4daed562ed7fa879838d790a5ad3be5fb3b0d
Max <msal.coding@gmail.com>
2021-03-20 23:20:32 +0100

    add some more example filters in debug build to test

--- app/src/main/kotlin/de/salomax/ndx/data/NdxDatabase.kt
+++ app/src/main/kotlin/de/salomax/ndx/data/NdxDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import de.salomax.ndx.BuildConfig
 import de.salomax.ndx.R
 import java.util.concurrent.Executors
 
@@ -39,21 +40,45 @@ abstract class NdxDatabase : RoomDatabase() {
             // pre-populate the database
             override fun onCreate(db: SupportSQLiteDatabase) {
                 super.onCreate(db)
-
                 // add some filters
+                val filters = mutableListOf(
+                        Filter(null,
+                                context.resources.getInteger(R.integer.preset_filterValue1),
+                                context.getString(R.string.preset_filterName1),
+                                context.getString(R.string.preset_filterInfo1)),
+                        Filter(null,
+                                context.resources.getInteger(R.integer.preset_filterValue3),
+                                context.getString(R.string.preset_filterName3),
+                                context.getString(R.string.preset_filterInfo3))
+                )
+                // add some more when in debug mode
+                if (BuildConfig.DEBUG) {
+                    filters.add(Filter(null,
+                            context.resources.getInteger(R.integer.preset_filterValue2),
+                            context.getString(R.string.preset_filterName2),
+                            context.getString(R.string.preset_filterInfo2))
+                    )
+                    filters.add(Filter(null,
+                            context.resources.getInteger(R.integer.preset_filterValue4),
+                            context.getString(R.string.preset_filterName4),
+                            context.getString(R.string.preset_filterInfo4))
+                    )
+                    filters.add(Filter(null,
+                            context.resources.getInteger(R.integer.preset_filterValue5),
+                            context.getString(R.string.preset_filterName5),
+                            context.getString(R.string.preset_filterInfo5))
+                    )
+                    filters.add(Filter(null,
+                            context.resources.getInteger(R.integer.preset_filterValue6),
+                            context.getString(R.string.preset_filterName6),
+                            context.getString(R.string.preset_filterInfo6))
+                    )
+                }
+                // insert
                 Executors.newSingleThreadScheduledExecutor().execute {
                     getInstance(context)
                             .filterDao()
-                            .insertAll(listOf(
-                                    Filter(null,
-                                            context.resources.getInteger(R.integer.preset_filterValue1),
-                                            context.getString(R.string.preset_filterName1),
-                                            context.getString(R.string.preset_filterInfo1)),
-                                    Filter(null,
-                                            context.resources.getInteger(R.integer.preset_filterValue3),
-                                            context.getString(R.string.preset_filterName3),
-                                            context.getString(R.string.preset_filterInfo3))
-                            ))
+                            .insertAll(filters)
                 }
             }
         }
